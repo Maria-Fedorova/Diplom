92aa53deb82488e02d8789f47c1058d27bbe8731
Maria-Fedorova <157302242+Maria-Fedorova@users.noreply.github.com>
2025-01-18 18:28:53 +0300

    Add CRUD diary

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from diary.models import Diary
+
+
+@admin.register(Diary)
+class DiaryAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content", "create_date","updated_date", "author",)
+    list_filter = ("author", "title", "create_date", "updated_date")
+    search_fields = ("author", "title",)
--- diary/views.py
+++ diary/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 
+from diary.forms import DiaryForm
 from diary.models import Diary
 
 
 class DiaryListView(ListView):
     model = Diary
+    form_class = DiaryForm
 
+
+class DiaryCreateView(CreateView):
+    model = Diary
+    form_class = DiaryForm
+    success_url = reverse_lazy("diary:diary_list")
+
+
+class DiaryUpdateView(UpdateView):
+    model = Diary
+    success_url = reverse_lazy("diary:diary_list")
+
+
+class DiaryDetailView(DetailView):
+    model = Diary
+
+
+class DiaryDeleteView(DeleteView):
+    model = Diary
+    success_url = reverse_lazy('diary:diary_list')
